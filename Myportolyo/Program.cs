6ccc248854ec8d3c4c7203b4453922486a56078b
Serkan Ozturk <Ozturksahib98@gmail.com>
2025-10-04 17:36:40 +0300

    program.cs kısmı düzeltidi.

--- Myportolyo/Program.cs
+++ Myportolyo/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=DefaultController1}/{action=Index}/{id?}");
 
 app.Run();
